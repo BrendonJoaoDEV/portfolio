58614d627c91be1e6d234ee529bd3bdfd41ea8d4
Brendon João <brendonjoaodev@gmail.com>
2025-07-29 00:35:39 -0300

    TEST: Teste da função definirTema

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { carregarElementoEstatico } from './utils/utils.js';
+import { definirTema } from './modules/mudanca_tema.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
     const header = document.getElementById('header-placeholder');
     const footer = document.getElementById('footer-placeholder');
+    
+    definirTema(body);
 
     carregarElementoEstatico('../partials/header.html', header);
     carregarElementoEstatico('../partials/footer.html', footer);
