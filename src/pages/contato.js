e08abf69a0c180c7b7d206b4fd3caca45fd2f4aa
Brendon João <brendonjoaodev@gmail.com>
2025-07-29 01:29:43 -0300

    FEAT: Aplicação das funções de tema nas outras páginas

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,9 +1,16 @@
 import { carregarElementoEstatico } from '../utils/utils.js';
+import { definirTema, mudarTema } from '../modules/mudanca_tema.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
     const header = document.getElementById('header-placeholder');
     const footer = document.getElementById('footer-placeholder');
+    const btnTema = document.getElementById('btn-tema');
+
+    definirTema(body);
 
     carregarElementoEstatico('../partials/header.html', header);
     carregarElementoEstatico('../partials/footer.html', footer);
+
+    btnTema.addEventListener('click', () => {mudarTema(body)});
 });
