835a90b3abb2a7a8c1d529a2fc8d58f250574050
Brendon João <brendonjoaodev@gmail.com>
2025-08-01 00:33:25 -0300

    FEAT: Validação dos campos do formulário

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,4 +1,4 @@
-import { carregarElementoEstatico } from '../utils/utils.js';
+import { carregarElementoEstatico, validarNome, validarEmail, validarVazio} from '../utils/utils.js';
 import { definirTema, mudarTema } from '../modules/mudanca_tema.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementById('header-placeholder');
     const footer = document.getElementById('footer-placeholder');
     const btnTema = document.getElementById('btn-tema');
+    const btnEnviar = document.getElementById('enviar-formulario');
+    const entradaNome = document.getElementById('entrada-nome');
+    const entradaEmail = document.getElementById('entrada-email');
+    const entradaMotivo = document.getElementById('entrada-motivo');
+    const entradaMensagem = document.getElementById('entrada-mensagem');
+    const erroNome = document.getElementById('erro-nome');
+    const erroEmail = document.getElementById('erro-email');
+    const erroMensagem = document.getElementById('erro-mensagem');
 
     definirTema(body);
 
@@ -13,4 +21,40 @@ document.addEventListener('DOMContentLoaded', () => {
     carregarElementoEstatico('../partials/footer.html', footer);
 
     btnTema.addEventListener('click', () => {mudarTema(body)});
+
+    btnEnviar.addEventListener('click', () => {
+        // Pegar valores dos campos de entrada
+        let nome = entradaNome.value;
+        let email = entradaEmail.value;
+        let motivo = entradaMotivo.value;
+        let mensagem = entradaMensagem.value;
+        
+        // Validar cada um desses valores
+        let validacao = validarNome(nome);
+        if (!validacao.valido) {
+            erroNome.style.display = 'block';
+            erroNome.textContent = validacao.erro;
+        } else {
+            erroNome.style.display = 'none';
+        }
+
+        validacao = validarEmail(email);
+        if (!validacao.valido) {
+            erroEmail.style.display = 'block';
+            erroEmail.textContent = validacao.erro;
+        } else {
+            erroEmail.style.display = 'none';
+        }
+
+        validacao = validarVazio('Mensagem', mensagem);
+        if (!validacao.valido) {
+            erroMensagem.style.display = 'block';
+            erroMensagem.textContent = validacao.erro;
+        } else {
+            erroMensagem.style.display = 'none';
+        }
+        // Validar se o usuário parece humano suficiente
+            // Se todos forem válidos e usuário humano: enviar dados estruturados a função de envio
+            // Se não, pedir para o usuário reinserir o dado invalido
+    });
 });
